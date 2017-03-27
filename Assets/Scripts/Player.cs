140097398265314720209e274c12d42feee007cd
Unknown <glarkin1996@gmail.com>
2017-03-27 10:47:19 -0400

    Added C# scripts for multiple types. Added ability for player to move.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int health = 100;
+	new RigidBody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<RigidBody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		rigidbody.velocity = new Vector2 (x, 0);
+	
+	}
+}
