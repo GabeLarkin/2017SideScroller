d0186b1e6b23251e8437d1feb9dda614f4c7e1af
Unknown <glarkin1996@gmail.com>
2017-05-15 12:32:21 -0400

    Attempted to make a warp pipe

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+    public Transform Destination;      
+    public string TagList = "|Player|Boss|Friendly|"; 
+
+    
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+       
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (TagList.Contains(string.Format("|{0}|", other.tag)))
+        {
+            
+            other.transform.position = Destination.transform.position;
+            
+        }
+    }
+}
